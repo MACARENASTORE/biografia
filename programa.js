f4bf678a87377754a6b5fb3b68d75710acd86443
Diego Barreto <oclusiva9204@gmail.com>
2023-03-02 14:34:17 -0500

    commit2

---
+++ programa.js
@@ -0,0 +1,21 @@
+//comentarios de una linea, clarificar una expresión
+
+/* 
+cometarios de 
+varias lineas
+documentar el codigo
+*/
+
+//No es necesario declarar una variable antes de utilizarlar
+//Variable: Un lugar en la memoria al cual le coloco nombre, 
+//y lo  uso para guardar un valor
+
+//Variables de ambito de función.
+var a;
+
+//Varialbes de ambito de bloque
+let b;
+
+//Operacion: aritmetica, comparacion, logica binaria (AND. OR)
+
+//Estructuras de datos: arreglos... guardan mas de una variable
